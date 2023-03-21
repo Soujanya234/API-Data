9016116491c0a82e361a657ba0f4cf0d0ca77705
Soujanya234 <Mandadapusoujanya24@gmail.com>
2023-03-21 10:16:11 +0530

    Initial Commit

---
+++ API Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API Data
+//
+//  Created by Soujanya Mandadapu on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
